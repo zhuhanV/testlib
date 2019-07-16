e34b541eedc858ad35e3890bfc29741d854d15c9
ViraZh <o2898765@ukr.net>
2019-07-16 17:17:43 +0300

    mavens

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,12 +1,35 @@
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
 
 public class TestMain {
     public static void main(String[] args) {
-        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi");
+        Properties prop = new Properties();
+        try (InputStream resourceAsStream = TestMain.class.getClassLoader()
+        .getResourceAsStream("config.properties")){
+prop.load(Objects.requireNonNull(resourceAsStream));
+        }
+        catch (IOException ex) {
+            System.err.println("Unable to load properties file: "+ "config.properties");
+        }
+
+        System.out.println(prop.get("props.local.hello"));
+        System.out.println(prop.get("props.mvn.hello"));
+
+               List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi");
         fruits.forEach(System.out::println);
         List<String> reverseFruits = Lists.reverse(fruits);
         reverseFruits.forEach(System.out::println);
+
+        Multimap<String, String> map = ArrayListMultimap.create();
+        map.put("key", "firstValue");
+        map.put("key", "secondValue");
+        System.out.println(map);
     }
 }
